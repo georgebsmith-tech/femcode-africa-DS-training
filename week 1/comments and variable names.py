012086ed1b3c40ae189c063dfb18df8fef9a20eb
George, B. Smith <georgebsmith.tech@gmail.com>
2020-09-29 00:06:15 -0700

    first commit

---
+++ week 1/comments and variable names.py
@@ -0,0 +1,15 @@
+#This is a section on comments and varaible names
+a=4 # = is an assignment operator
+b=6
+c=9
+sum_of_numbers = a+b+c # this sums the 3 numbers
+#print(sum_of_numbers)
+
+
+total_seconds=2000
+minutes=total_seconds//60
+seconds=total_seconds%60
+#print(minutes)
+#print(seconds)
+
+print(minutes,seconds)
